dde2079c84dfac757540689feb36f336ad303509
Tobias Schaefer <tobias.schaefer@novatec-gmbh.de>
2016-09-12 21:26:52 +0200

    git clone

--- documentation.sh
+++ documentation.sh
@@ -1,22 +1,24 @@
 #!/usr/bin/env sh
-echo "##########################################################################################"
-echo "hello world!"
-echo "##########################################################################################"
-echo "hello world!"
-echo "##########################################################################################"
-echo "hello world!"
-echo "##########################################################################################"
-echo "hello world!"
-echo "##########################################################################################"
-echo "hello world!"
-echo "##########################################################################################"
-echo "hello world!"
-echo "##########################################################################################"
-echo "hello world!"
-echo "##########################################################################################"
-echo "hello world!"
-echo "##########################################################################################"
-echo "hello world!"
-echo "##########################################################################################"
-echo "hello world!"
-echo "##########################################################################################"
+set -e # Exit with nonzero exit code if anything fails
+
+echo "Uploading documentation"
+
+REPO = "https://github.com/tobiasschaefer/tobiasschaefer.github.io.git"
+DIRECTORY = "documentation"
+
+git clone $REPO $DIRECTORY
+
+# Clean out existing contents
+rm -rf $DIRECTORY/**/*
+
+cp -r target/site/serenity/* $DIRECTORY
+
+cd $DIRECTORY
+git config user.name "Travis CI"
+git config user.email "$COMMIT_AUTHOR_EMAIL"
+
+git status
+
+git diff
+
+
