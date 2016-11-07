a52e3e779e60e97c721f990515a88eb887518d69
Thorsten Jakoby <tj@novatec-gmbh.de>
2016-11-07 18:22:10 +0100

    ECM-45: auto config educama user to camunda configuration - dev only

--- backend/src/main/java/org/educama/EducamaApplication.java
+++ backend/src/main/java/org/educama/EducamaApplication.java
@@ -2,9 +2,16 @@ package org.educama;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.camunda.bpm.spring.boot.starter.SpringBootProcessApplication;
 
+/*
+ * Note that extending SpringBootProcessApplication may cause that some Camunda-auto-detect-features won't work.
+ * For example define a process.xml will be necessary in some cases.
+ * In general working with camunda-bpm-spring-boot-starter-webapp will be more according to Camunda documentation
+ * instead of camunda-bpm-spring-boot-starter-webapp documentation.
+ */
 @SpringBootApplication
-public class EducamaApplication {
+public class EducamaApplication extends SpringBootProcessApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EducamaApplication.class, args);
