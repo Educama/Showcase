0b993d8ad4b75b3de26d0c8cd199c5b54b2bd038
Lars Duelfer <lars.duelfer@novatec-gmbh.de>
2016-11-07 22:12:57 +0100

    ECM-46: Create Backend Example

---
+++ backend/src/main/java/org/educama/shipment/api/resource/ShipmentListResource.java
@@ -0,0 +1,19 @@
+/*
+ * *************************************************************************
+ *
+ * Copyright:       Robert Bosch GmbH, 2016
+ *
+ * *************************************************************************
+ */
+
+/**
+ * Dear "developer", please document your class RIGHT HERE!.
+ */
+
+package org.educama.shipment.api.resource;
+
+import java.util.List;
+
+public class ShipmentListResource {
+    public List<ShipmentResource> shipments;
+}
---
+++ backend/src/main/java/org/educama/shipment/api/resource/ShipmentResource.java
@@ -0,0 +1,20 @@
+/*
+ * *************************************************************************
+ *
+ * Copyright:       Robert Bosch GmbH, 2016
+ *
+ * *************************************************************************
+ */
+
+/**
+ * Dear "developer", please document your class RIGHT HERE!.
+ */
+
+package org.educama.shipment.api.resource;
+
+public class ShipmentResource {
+
+    public long id;
+    public String senderAdress;
+    public String receiverAdress;
+}
