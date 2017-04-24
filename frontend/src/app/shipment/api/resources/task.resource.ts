babbb307a4cebaeb8a53c4fbadfea87891bd1430
Doris Weggenmann <doris.weggenmann@web.de>
2017-04-24 18:18:07 +0200

    ECM-69

--- frontend/src/app/shipment/api/resources/task.resource.ts
+++ frontend/src/app/shipment/api/resources/task.resource.ts
@@ -1,9 +1,12 @@
+import {Party} from "../datastructures/party.datastructure";
 export class TaskResource {
     public createTime: Date;
-    public trackingId: string;
+    public trackingId: number;
     public taskId: string;
     public name: string;
     public description: string;
     public assignee: string;
     public customer: string;
+    public sender: Party;
+    public receiver: Party;
 }
