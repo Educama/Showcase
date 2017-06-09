9ce3757141607e60653773bd7ce10252e10c63c7
Jose Lara <jose.lara@novatec-gmbh.de>
2017-06-09 12:12:27 +0200

    Merging origin with Supplier Simulator services

--- EventService/app/models/event.js
+++
@@ -1,8 +0,0 @@
-var mongoose     = require('mongoose');
-var Schema       = mongoose.Schema;
-
-var EventSchema   = new Schema({
-    id: String
-});
-
-module.exports = mongoose.model('Event', EventSchema);
--- EventSimulator/src/app/event-form.ts
+++
@@ -1,3 +0,0 @@
-export class EventForm {
-    id: string;
-}
