0ad84c8e97dff0fb015400fcf249b902c5ea44f7
Eberhard Heber <eberhard.heber@novatec-gmbh.de>
2017-07-11 08:30:59 +0200

    ECM-173: Added private camunda repo to travis build.

--- backend/build.sh
+++ backend/build.sh
@@ -5,7 +5,7 @@ START_TIME=$SECONDS
 
 echo -n "Java Version: " && java -version
 echo -n "Javac Version: " javac -version
-./mvnw install
+./mvnw install -P enterprise --settings ../settings.xml
 
 echo "Directory content after build:"
 ls -al
